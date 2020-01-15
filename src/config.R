186b42752aa09de4cb944133d6622a50b704067a
Ava Hoffman <avamariehoffman@gmail.com>
2020-01-15 21:58:34 +0100

    Add new script to compare gracilis biomass at Sev and SGS

--- src/config.R
+++ src/config.R
@@ -4,9 +4,11 @@
 
 
 ## General
+
 # Set working directory for the repository (should be the git repo):
 wd <-
   "/Users/avahoffman/Dropbox/Research/EDGE_Science/EDGE-science/"
+
 # Directories for specific outputs/inputs:
 data_dir <- paste(toString(wd), "data/", sep = "")
 figure_dir <- paste(toString(wd), "figures/", sep = "")
@@ -16,14 +18,19 @@ write_dir <- paste(toString(wd), "output/", sep = "")
 shortgrass_color <- "#CC0000"
 northern_color <- "#0000CC"
 desert_color <- "#CC9900"
+
 # Ecoregion colors (pale):
 shortgrass_color_pale <- "#F5D6D6"
 northern_color_pale <- "#D4E0F7"
 desert_color_pale <- "#FFE6CC"
 
+# Drought treatments to include in effect:
+include_in_drt_trt <- c("chr")
+
 
 ###########################################################################################
 ## Map
+
 # Colors for points representing these sites
 CHY_color <- "#CC0000"
 SGS_color <- "#0000CC"
@@ -32,10 +39,12 @@ SEV_Black_color <- "#FF9900"
 
 # Directory where map shapefiles representing ecoregions should be:
 shapefile_dir <- paste(toString(wd), "data/shapefiles/", sep = "")
+
 # Names for ecoregions:
 shortgrass_name <- "Shortgrass Steppe"
 northern_name <- "Northern Mixed Grass"
 desert_name <- "Desert Grassland"
+
 # Colors for ecoregions:
 climate_colors <-
   c(desert_color_pale,
@@ -45,47 +54,36 @@ climate_colors <-
 
 ###########################################################################################
 ## C4 vs C3 grass coverage
+
 # Sites to include:
 c4_c3_sites <- c("CHY", "SGS")
+
 # C4 or C3 Grasses to include:
 c4_grasses <- c("BOGR", "BOHI", "BUDA", "C4")
 c3_grasses <- c("CAREX", "PASM", "C3")
+
 # Years to include:
 c3_c4_years <- c(2017, 2016, 2015, 2014)
-# Drought treatments to include in effect:
-include_in_drt_trt <- c("chr")
 
 
 ###########################################################################################
 ## Bouteloua eripoda v gracilis coverage at Sevilleta sites 
+
 # Sites to include:
 eri_grac_sites <- c("SEV.blue", "SEV.black")
+
 # Years to include:
 eri_grac_years <- c(2017, 2016, 2015, 2014)
-# Grasses to include, especially in "other C4":
-eri_grac_grasses <- c(
-  "ARAD",
-  "ARPU",
-  "BOBA2",
-  "BOER4",
-  "BOGR",
-  "DAPU7",
-  "ENDE",
-  "MUAR2",
-  "MUSQ3",
-  "PLJA",
-  "SCBR2",
-  "SPCO4",
-  "SPCR",
-  "SPFL2",
-  "SPORO",
-  "ANGE",
-  "BOCU",
-  "BOHI",
-  "BUDA",
-  "SPAS",
-  "C4"
-)
+
+
+###########################################################################################
+## Bouteloua gracilis comparison at SGS and Sevilleta sites 
+
+# Sites to include:
+sev_sgs_sites <- c("SEV.blue", "SGS")
+
+# Years to include:
+sev_sgs_years <- c(2017, 2016, 2015, 2014)
 
 
 ###########################################################################################
--- src/sev_v_sgs.R
+++ src/sev_v_sgs.R
@@ -10,7 +10,7 @@ library(reshape2)
 ###########################################################################################
 
 
-collect_sev_data <-
+collect_sev_sgs_data <-
   function(ambient_composition, sum_across_years = TRUE) {
     # Sum across years option decides whether each year is a data point for site (FALSE), plot
     # OR whether to add up all the years cumulatively first (TRUE)
@@ -19,33 +19,55 @@ collect_sev_data <-
     raw_dat <- read.csv("EDGE_biomass_long_QAQC_final.csv")
     
     # Filter out old years
-    dat <- raw_dat[(raw_dat$Year %in% eri_grac_years),]
+    dat <- raw_dat[(raw_dat$Year %in% sev_sgs_years), ]
     
     # Lump all experimental droughts into drought (if want to exclude one or the other, see config)
-    dat <- dat[(dat$Trt %in% c(include_in_drt_trt, "con")),]
+    dat <- dat[(dat$Trt %in% c(include_in_drt_trt, "con")), ]
     dat <- dat %>%
-      mutate(Trt = as.character(Trt)) %>% mutate(Trt = replace(Trt, Trt == "chr" |
-                                                                 Trt == "int", "drt"))
+      mutate(Trt = as.character(Trt)) %>%
+      mutate(Trt = replace(Trt,
+                           Trt == "chr" |
+                             Trt == "int",
+                           "drt"))
     
     # Collect only C4 grasses
-    c4_taxa <- read.csv("Taxa_info.csv") %>% 
-      filter(Photo.path == "C4" & Funct.grp == "grass") %>% 
+    c4_taxa <-
+      read.csv("Taxa_info.csv") %>%
+      filter(Photo.path == "C4" &
+               Funct.grp == "grass") %>%
       pull(Plant.code)
-    c4_dat <- as_tibble(dat[(dat$category %in% c4_taxa),])
-    # Exclude Bouteloua gracilis and eriopoda, and describe as C4
+    c4_dat <- as_tibble(dat[(dat$category %in% c4_taxa), ])
+    # Exclude Bouteloua gracilis and describe as C4
     c4_dat_other <-
-      c4_dat %>% filter(category != "BOGR" &
-                          category != "BOER4") %>% mutate(category = replace(category,!(is.na(category)), "C4"))
-    # Save B. gracilis and eriopoda seperately
-    c4_dat_main <-
-      c4_dat %>% filter(category == "BOGR" | category == "BOER4")
+      c4_dat %>% filter(category != "BOGR") %>%
+      mutate(category = replace(category,
+                                !(is.na(category)),
+                                "C4"))
+    
+    # Collect only C3 grasses
+    c3_taxa <-
+      read.csv("Taxa_info.csv") %>%
+      filter(Photo.path == "C3" &
+               Funct.grp == "grass") %>%
+      pull(Plant.code)
+    c3_dat <- as_tibble(dat[(dat$category %in% c3_taxa), ])
+    # Describe as C3
+    c3_dat_other <-
+      c3_dat %>%
+      mutate(category = replace(category,
+                                !(is.na(category)),
+                                "C3"))
+    
+    # Save B. gracilis seperately
+    bogr_dat <-
+      c4_dat %>% filter(category == "BOGR")
     # Combine data segments
-    c4_dat <- rbind(c4_dat_other, c4_dat_main)
+    bogr_dat <- rbind(c4_dat_other, c3_dat_other, bogr_dat)
     
     # IF cumulative, sum across all years to get a more stable number
     if (sum_across_years) {
       full_dat <-
-        c4_dat %>% group_by(Site, Block, Plot, Trt, category) %>% summarise(biomass = sum(biomass) / length(eri_grac_years))
+        bogr_dat %>% group_by(Site, Block, Plot, Trt, category) %>% summarise(biomass = sum(biomass) / length(sev_sgs_years))
     }
     
     # Calculate total biomass
@@ -56,26 +78,26 @@ collect_sev_data <-
       dcast(full_dat, Site + Block + Plot + Trt ~ category, value.var = "biomass")
     # Join total to wide format and replace any NAs with zeros
     full_dat <-
-      full_join(totals, wide_dat, by = c("Site", "Block", "Plot", "Trt")) %>% 
-      mutate(BOER4 = replace(BOER4, (is.na(BOER4)), 0)) %>%
+      full_join(totals, wide_dat, by = c("Site", "Block", "Plot", "Trt")) %>%
       mutate(BOGR = replace(BOGR, (is.na(BOGR)), 0)) %>%
-      mutate(C4 = replace(C4, (is.na(C4)), 0))
+      mutate(C4 = replace(C4, (is.na(C4)), 0)) %>%
+      mutate(C3 = replace(C3, (is.na(C3)), 0))
     
     if (ambient_composition) {
       # Convert to percent of total
-      for (i in c("BOER4", "BOGR", "C4")) {
+      for (i in c("BOGR", "C4", "C3")) {
         full_dat[[i]] <- 100 * full_dat[[i]] / full_dat$total_biomass
       }
       # Keep only control plots
       full_dat <- full_dat %>% filter(Trt == "con")
       # Make long format
-      long_dat <- full_dat %>% gather(spp, pct, BOER4:C4)
+      long_dat <- full_dat %>% gather(spp, pct, BOGR:C4)
       
       # Summarize by site
       summary_dat <- long_dat %>% group_by(Site, spp) %>%
         summarise(mean = mean(pct),
-                  se = sd(pct) / sqrt(n())) %>% 
-        filter(Site %in% eri_grac_sites)
+                  se = sd(pct) / sqrt(n())) %>%
+        filter(Site %in% sev_sgs_sites )
       
       setwd(wd)
       return(summary_dat)
@@ -96,9 +118,9 @@ collect_sev_data <-
       
       # Subset based on species
       BOER4 <-
-        compare_dat %>% filter(Site == "SEV.black") %>% filter(BOER4.x > 0) %>% select(-BOGR.x, -BOGR.y)
+        compare_dat %>% filter(Site == "SEV.black") %>% filter(BOER4.x > 0) %>% select(-BOGR.x,-BOGR.y)
       BOGR <-
-        compare_dat %>% filter(Site == "SEV.blue") %>% filter(BOGR.x > 0) %>% select(-BOER4.x, -BOER4.y)
+        compare_dat %>% filter(Site == "SEV.blue") %>% filter(BOGR.x > 0) %>% select(-BOER4.x,-BOER4.y)
       
       # Calculate the difference
       BOGR$diff <-
@@ -131,7 +153,7 @@ collect_sev_data <-
   }
 
 
-plot_spp_sev <- function(summary_dat, filename = NA) {
+plot_spp_sev_sgs <- function(summary_dat, filename = NA) {
   gg <- ggplot(data = summary_dat) +
     
     # Draw bars
@@ -145,7 +167,8 @@ plot_spp_sev <- function(summary_dat, filename = NA) {
     
     # Add standard error for BOGR
     geom_errorbar(
-      data = summary_dat %>% filter(spp == "BOER4"),
+      data = summary_dat %>% 
+        filter(spp == "BOGR"),
       aes(
         x = Site,
         ymin = mean - se,
@@ -155,14 +178,15 @@ plot_spp_sev <- function(summary_dat, filename = NA) {
       width = 0
     ) +
     
-    # Add standard error for BOER4 - not a good workaround for this - considered bad practice to have
+    # Add standard error for C3 - not a good workaround for this - considered bad practice to have
     # stacked error bars
     geom_errorbar(
-      data = summary_dat %>% filter(spp == "BOGR"),
+      data = summary_dat %>% 
+        filter(spp == "C3"),
       aes(
         x = Site,
-        ymin = mean + 31.5 - se,
-        ymax = mean + 31.5 + se
+        ymin = mean + 58.4 - se,
+        ymax = mean + 58.4 + se
       ),
       size = 1,
       width = 0
@@ -191,8 +215,8 @@ plot_spp_sev <- function(summary_dat, filename = NA) {
       legend.title = element_blank(),
     ) +
     scale_fill_manual(
-      values = c(northern_color_pale, shortgrass_color_pale, "white"),
-      labels = c("B. eriopoda", "B. gracilis", "Other C4")
+      values = c(shortgrass_color_pale, northern_color_pale, "white"),
+      labels = c("B. gracilis", "C3 grasses", "Other C4")
     )
   
   gg
