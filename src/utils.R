a405afa934f5187ce0d43afaf3c8512be1b57c67
Ava Hoffman <avamariehoffman@gmail.com>
2020-01-29 15:47:18 -0500

    Format utils

--- src/utils.R
+++ src/utils.R
@@ -11,35 +11,36 @@ theme_sigmaplot <- function(xticks = TRUE,
   # Use as an additional arg, eg:
   # ggplot() + theme_sigmaplot()
   
-  sigmaplot <- theme(
-    panel.background = element_blank(),
-    panel.border = element_rect(size = 1, fill = NA),
-    legend.key = element_rect(fill = NA),
-    axis.ticks.length.y = unit(ticklen, "cm"),
-    axis.ticks.length.y.right = unit(ticklen, "cm"),
-    axis.ticks.length.x = unit(ticklen, "cm"),
-    axis.text.x = element_text(
-      color = "black",
-      margin = margin(
-        t = 10,
-        r = 0,
-        b = 0,
-        l = 0,
-        unit = "pt"
-      )
-    ),
-    axis.text.y = element_text(
-      hjust = 1,
-      color = "black",
-      margin = margin(
-        t = 0,
-        r = 10,
-        b = 0,
-        l = 0,
-        unit = "pt"
-      )
-    ),
-  )
+  sigmaplot <-
+    theme(
+      panel.background = element_blank(),
+      panel.border = element_rect(size = 1, fill = NA),
+      legend.key = element_rect(fill = NA),
+      axis.ticks.length.y = unit(ticklen, "cm"),
+      axis.ticks.length.y.right = unit(ticklen, "cm"),
+      axis.ticks.length.x = unit(ticklen, "cm"),
+      axis.text.x = element_text(
+        color = "black",
+        margin = margin(
+          t = 10,
+          r = 0,
+          b = 0,
+          l = 0,
+          unit = "pt"
+        )
+      ),
+      axis.text.y = element_text(
+        hjust = 1,
+        color = "black",
+        margin = margin(
+          t = 0,
+          r = 10,
+          b = 0,
+          l = 0,
+          unit = "pt"
+        )
+      ),
+    )
   if (!xticks) {
     sigmaplot <- sigmaplot +
       theme(axis.ticks.x = element_blank())
