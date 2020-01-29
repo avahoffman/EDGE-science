ac751edd3471515d7354e172f641dc367f2d4c72
Ava Hoffman <avamariehoffman@gmail.com>
2020-01-29 15:54:44 -0500

    Format config

--- src/config.R
+++ src/config.R
@@ -12,9 +12,18 @@ wd <-
 #   "/Users/avahoffman/Dropbox/Research/EDGE_Science/EDGE-science/"
 
 # Directories for specific outputs/inputs:
-data_dir <- paste(toString(wd), "data/", sep = "")
-figure_dir <- paste(toString(wd), "figures/", sep = "")
-write_dir <- paste(toString(wd), "output/", sep = "")
+data_dir <-
+  paste(toString(wd),
+        "data/",
+        sep = "")
+figure_dir <-
+  paste(toString(wd),
+        "figures/",
+        sep = "")
+write_dir <-
+  paste(toString(wd),
+        "output/",
+        sep = "")
 
 ## Colors
 
@@ -45,12 +54,21 @@ sgs_grac_color <- "#fdae6b"
 # Drought treatments to include in effect:
 include_in_drt_trt <- c("chr")
 
+# Years to take into account:
+experiment_years <-
+  c(2017,
+    2016,
+    2015,
+    2014)
 
 ###########################################################################################
 ## Map and Huxman data with inset (sensitivity)
 
 # Directory where map shapefiles representing ecoregions should be:
-shapefile_dir <- paste(toString(wd), "data/shapefiles/", sep = "")
+shapefile_dir <-
+  paste(toString(wd),
+        "data/shapefiles/",
+        sep = "")
 
 # Names for ecoregions:
 shortgrass_name <- "Shortgrass Steppe"
@@ -64,13 +82,21 @@ climate_colors <-
     shortgrass_color)
 
 # Years for inset sensitivity
-sensitivity_years <- c(2017, 2016, 2015, 2014)
-sensitivity_sites <- c("CHY","SGS","SEV.blue","SEV.black")
+sensitivity_years <- experiment_years
+sensitivity_sites <-
+  c("CHY",
+    "SGS",
+    "SEV.blue",
+    "SEV.black")
 
 # Figure details
 #y_lab_inset <- "Percent Decline in Total ANPP"
 y_lab_inset <- NULL
-x_ticks_inset <- c("SEV\nBlack", "SEV\nBlue", "SGS", "CHY")
+x_ticks_inset <-
+  c("SEV\nBlack",
+    "SEV\nBlue",
+    "SGS",
+    "CHY")
 
 
 ###########################################################################################
@@ -80,63 +106,82 @@ x_ticks_inset <- c("SEV\nBlack", "SEV\nBlue", "SGS", "CHY")
 c4_c3_sites <- c("CHY", "SGS")
 
 # C4 or C3 Grasses to include:
-c4_grasses <- c("BOGR", "BOHI", "BUDA", "C4")
-c3_grasses <- c("CAREX", "PASM", "C3")
+c4_grasses <-
+  c("BOGR",
+    "BOHI",
+    "BUDA",
+    "C4")
+c3_grasses <-
+  c("CAREX",
+    "PASM",
+    "C3")
 
 # Years to include:
-c3_c4_years <- c(2017, 2016, 2015, 2014)
+c3_c4_years <- experiment_years
 
 # Figure details
-x_ticks_3 <- c("CHY","SGS")
+x_ticks_3 <- c("CHY", "SGS")
 y_lab_3 <- "% of Grass ANPP"
 legend_names_3 <- c("C3 Grasses", "C4 Grasses")
 y_lab_4 <- "% Change in ANPP"
 
 
 ###########################################################################################
-## Bouteloua eripoda v gracilis coverage at Sevilleta sites 
+## Bouteloua eripoda v gracilis coverage at Sevilleta sites
 
 # Sites to include:
 eri_grac_sites <- c("SEV.blue", "SEV.black")
 
 # Years to include:
-eri_grac_years <- c(2017, 2016, 2015, 2014)
-
+eri_grac_years <- experiment_years
 
 # Figure details
 x_ticks_5 <- c("SEV Black", "SEV Blue")
 y_lab_5 <- "% of Grass ANPP"
-legend_names_5 <- c(expression(~italic("B. eriopoda")),
-                    expression(~italic("B. gracilis")),
-                    "Other C4")
-y_lab_6 <- "% Change in ANPP" 
-legend_names_6 <- c(expression(~italic("B. eriopoda")),
-                   expression(~italic("B. gracilis")))
+legend_names_5 <-
+  c(expression(~ italic("B. eriopoda")),
+    expression(~ italic("B. gracilis")),
+    "Other C4")
+y_lab_6 <- "% Change in ANPP"
+legend_names_6 <-
+  c(expression(~ italic("B. eriopoda")),
+    expression(~ italic("B. gracilis")))
 
 
 ###########################################################################################
-## Bouteloua gracilis comparison at SGS and Sevilleta sites 
+## Bouteloua gracilis comparison at SGS and Sevilleta sites
 
 # Sites to include:
 sev_sgs_sites <- c("SEV.blue", "SGS")
 
 # Years to include:
-sev_sgs_years <- c(2017, 2016, 2015, 2014)
+sev_sgs_years <- experiment_years
 
 # Figure details
 x_ticks_7 <- c("SEV Blue", "SGS")
 y_lab_7 <- "% of Grass ANPP"
-legend_names_7 <- c(expression(~italic("B. gracilis")),
-                    "C3 Grasses",
-                    "Other C4")
-y_lab_8 <- expression(paste("% Change in ",italic("B. gracilis"), " Biomass (g)"))
+legend_names_7 <-
+  c(expression( ~ italic("B. gracilis")),
+    "C3 Grasses",
+    "Other C4")
+y_lab_8 <-
+  expression(paste("% Change in ",
+                   italic("B. gracilis"),
+                   " Biomass (g)"))
 
 ######################################################s#####################################
 ## Genetic comparison
 # Very important not to mess around with these unless cross validation has been performed!!
 genetic_data <-
-  paste(toString(wd), "data/genetic/genind_all.R", sep = "")
-genetic_pops_to_use <- c("SGS", "Sevilleta", "Konza", "Cedar Point")
+  paste(toString(wd),
+        "data/genetic/genind_all.R",
+        sep = "")
+
+genetic_pops_to_use <-
+  c("SGS",
+    "Sevilleta",
+    "Konza",
+    "Cedar Point")
 DAPC_colors_pale <-
   c(sgs_grac_color,
     sev_grac_color,
@@ -148,8 +193,13 @@ n_prin_comp <- 15
 # Figure details
 x_lab_9 <- "LD1"
 y_lab_9 <- "LD2"
-legend_names_9 <- c("NB", "KNZ", "SEV Blue", "SGS")
-y_lab_10 <- expression(paste(italic("B. gracilis"), " Biomass (g)"))
+legend_names_9 <-
+  c("NB",
+    "KNZ",
+    "SEV Blue",
+    "SGS")
+y_lab_10 <-
+  expression(paste(italic("B. gracilis"),
+                   " Biomass (g)"))
 x_ticks_10 <- c("Dry", "Wet")
 legend_names_10 <- c("SEV Blue", "SGS")
-
