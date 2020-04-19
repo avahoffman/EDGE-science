3b76d83b7308e833091a517f3840f679fe6aa1d3
Ava Hoffman <avamariehoffman@gmail.com>
2020-04-19 11:47:51 -0400

    Add code to save discarded outliers in a csv

--- src/outlier_analysis.R
+++ src/outlier_analysis.R
@@ -12,6 +12,7 @@ make_outlier_plot <-
     # This function will test for chi-square scores that are outside the
     # percentile cutoff, and color them blue.
     # For best results, use only on a specific site-category-treatment subset
+    # Probably best for viz only!!
     ggplot() +
       geom_point(aes(
         x = as.numeric(rownames(d)),
@@ -31,33 +32,49 @@ remove_outliers <-
            percentile_cutoff = 0.99,
            outlier_prop_threshold = 0.5) {
     # Find outliers by Site, Trt, and species/category
-    dat = data.frame()
+    
+    # Dummy data frames
+    dat <-  data.frame()
+    dropped_dat <- data.frame()
+    
     # First divide up by site and drop that column
     for (Site_ in distinct(df, Site)$Site) {
       site_level_dat <-
         df %>%
         filter(Site == Site_)
+      
       # Divide up by treatment and drop that column
       for (Trt_ in distinct(site_level_dat, Trt)$Trt) {
         trt_level_dat <-
           site_level_dat %>%
           filter(Trt == Trt_)
+        
         # Divide up by species and drop that column
         for (category_ in distinct(trt_level_dat, category)$category) {
           cat_level_dat <-
             trt_level_dat %>%
             filter(category == category_)
+          
           # Perform chi-square test for outliers
           influential <-
             outliers::scores(cat_level_dat$biomass,
                              type = "chisq",
                              prob = percentile_cutoff)
+          
           # Only drop influential points if the proportion of influential points is less than some threshold
           # (eg, allows you to only get rid of data if a very small proportion are outliers)
           if ((length(influential[influential]) / nrow(cat_level_dat)) <= outlier_prop_threshold) {
-            d <- cat_level_dat[!(influential),] # drop any where they are not influential
+            d <-
+              cat_level_dat[!(influential), ] # drop any where they are not influential
+            dropped_d <-
+              cat_level_dat[(influential), ] # Keep dropped values for double checking
+            
+            # Bind to new data frames
             dat <- rbind(dat, d)
+            dropped_dat <- rbind(dropped_dat, dropped_d)
+            
           } else {
+            # Simply bind if no outliers
             dat <- rbind(dat, cat_level_dat)
           }
         }
@@ -84,21 +101,26 @@ remove_outliers <-
       summarise(biomass = mean(biomass))
     
     # Replace BOGR2 with BOGR for continuity
-    aggregated_dat <- 
+    aggregated_dat <-
       aggregated_dat %>%
       mutate(category = replace(category, category == "BOGR2", "BOGR"))
     
     # Remove noted outliers
     if (exclude_SGS_2016_plot2) {
       # there's a note that states it was grazed by cattle (BOGR, Carex, and BUDA are 0 across all iterations)
-      aggregated_dat <- 
-        aggregated_dat %>% 
+      aggregated_dat <-
+        aggregated_dat %>%
         filter(!(Site == "SGS" & Year == 2016 & Plot == 2))
     }
     
     # Write data
-    write.csv(aggregated_dat, 
+    write.csv(aggregated_dat,
               "data/EDGE_biomass_outliers_removed.csv",
+              row.names = FALSE)
+    
+    # Write the data that's dropped for double checking
+    write.csv(dropped_dat,
+              "data/EDGE_biomass_DROPPED.csv",
               row.names = FALSE)
     
     return(aggregated_dat)
